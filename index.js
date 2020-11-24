f952e76006fa3d2a7b708696bbad5edf7636e8b5
Rodrigo Faria <rodrigolfsi@gmail.com>
2020-11-24 06:18:04 -0300

    feature: printing the content of the body received

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const Koa = require('koa')
+const KoaBody = require('koa-body')
 const router = require('./api/v1/src/routes.config')
 
 const app = new Koa()
 
+app.use(KoaBody())
 app.use(router.routes())
 
 app.listen(3000, () => console.log('Server started and listening port 3000'))
