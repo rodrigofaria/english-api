383d0545f4280068b4232caaa3927956a34dcfbe
Rodrigo Faria <rodrigolfsi@gmail.com>
2020-11-24 07:09:36 -0300

    feature: testing reading data from body

--- api/v1/src/controller/english.controller.js
+++ api/v1/src/controller/english.controller.js
@@ -4,14 +4,21 @@ const test = ctx => {
   console.log('Inside the request body:')
   console.log(ctx.request.body)
 
-
   try {
+    const chatMessage = ctx.request.body
+    const chat_id = chatMessage.message.chat.id
+    const userName = chatMessage.message.from.first_name
+
+    const url = `https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage?chat_id=${chat_id}&text=Olá ${userName}`
+    console.log('URL to send POST: ', url)
+
     console.log(process.env)
     console.log('NOW:')
     console.log(process.env.BOT_TOKEN)
   } catch(err) {
     console.log('Error to read env var: ', err)
   }
+
 }
 
 const test2 = ctx => {
