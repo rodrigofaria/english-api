b0f97302c30ccc546a40c9770a492262c758bdb2
Rodrigo Faria <rodrigolfsi@gmail.com>
2020-12-08 08:24:21 -0300

    feature: adding sequelize to project

--- index.js
+++ index.js
@@ -11,3 +11,18 @@ app.use(router.routes())
 const port = process.env.PORT | 3000
 
 app.listen(port, () => console.log(`Server started and listening port ${port}`))
+
+
+test(index.sequelize)
+
+const test = async seq => {
+  try {
+    console.log('RODRIGO 1.0')
+    await seq.authenticate()
+    console.log('RODRIGO 1.1')
+  } catch (err) {
+    console.log('RODRIGO 2.0')
+    console.log(err)
+  }
+  console.log('RODRIGO 2')
+}
