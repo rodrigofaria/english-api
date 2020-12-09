edc2e8f06b0910e48521c42858dab3e394f3cebc
Rodrigo Faria <rodrigolfsi@gmail.com>
2020-12-09 08:44:04 -0300

    fix cloud sql connection

--- config/index.js
+++ config/index.js
@@ -2,6 +2,7 @@ module.exports = {
   username: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
-  host: process.env.DATABASE_HOST,
+  //host: process.env.DATABASE_HOST,
+  host: `${process.env.DB_SOCKET_PATH}/${process.env.CLOUD_SQL_CONNECTION_NAME}`,
   dialect: 'postgres'
 }
